9884c5ffcda56ce7ce603e7259ad8dd50a12eb25
amoltiw <amoltiw.eternity@gmail.com>
2025-02-11 10:43:33 +0530

    Update and rename Application2.sql to Application1_hist.sql

--- Application1_hist.sql
+++ Application1_hist.sql
@@ -1,4 +1,4 @@
-REPLACE PROCEDURE tes_db.test_state
+REPLACE PROCEDURE tes_db.test_state_hist
 
  ------------------------------------------------------------------------------------------
 --              				 test_state                         	                  --
@@ -6,10 +6,10 @@ REPLACE PROCEDURE tes_db.test_state
 -- DBMS Name        :  Snowflake                                                      --
 -- Database Name    :  test_db                                         			        --
 -- Applipation      : test_state                                                  	    --
--- Program Name     :  application2.sql                               	          --
+-- Program Name     :  application1_hist.sql                               	          --
 -- Description      :  Load the DATA TO cube  table                	  --
 -- Job Name         : NA                 --
--- Final Table Name : test_snowflakedb.target_test_table                      	           --
+-- Final Table Name : test_snowflakedb.target_test_hist_table                    	           --
 ------------------------------------------------------------------------------------------
 
 BEGIN
@@ -19,26 +19,25 @@ BEGIN
  DECLARE create_dt            DATE;
  DECLARE new_create_dt           TIMESTAMP(0);
  
- del from itsp_amr.wx_wallet_entry_id;
 
-insert into test_db.stage1
+insert into test_db.hist_stage1
 sel  a,b,c
-  from  src_db.source1 a, src_db.source2  b
+  from  src_db.source_hist1 a, src_db.source_hist2  b
   where a.id = b.id  ;
 
 
 
-insert into test_db.stage2
+insert into test_db.hist_stage2
 select a,b,c
 
-from src_db.source3 
+from src_db.hist_source3 
 
-insert into test_db.stage3
+insert into test_db.hist_stage3
 select a,b,c
 
-from src_db.source4
+from src_db.hist_source4
 
-update test_db.stage3
-from  src_db.source5
+update test_db.hist_stage3
+from  src_db.hist_source5
 set
 id = b.abc_id
